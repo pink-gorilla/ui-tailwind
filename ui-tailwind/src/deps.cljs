a5f6fff4a9201ac321ab05f0d0a7ec30fff36c11
awb99 <awb99@gmail.com>
2023-08-14 16:27:46 -0500

    deps.cljs fixed

--- ui-tailwind/src/deps.cljs
+++ ui-tailwind/src/deps.cljs
@@ -2,8 +2,10 @@
  {; shadow cljs creates package.json 
   ; based on this dependencies 
 
-  ; video
-  "react-player" "^2.9.0"
+  "tailwindcss" "2.1.2"
+  "autoprefixer" "^10.0.2" ; peer dependency of tailwind. actually needed?
+  "postcss" "^8.1.13"  ; peer dependency of tailwind
+
 
  ;
   }}
