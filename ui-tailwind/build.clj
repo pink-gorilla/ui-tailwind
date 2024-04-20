6621c981dbf5cafe9ec9fa60e7eb46de8499e7e4
awb99 <awb99@gmail.com>
2024-04-19 19:39:45 -0500

    ci license

--- ui-tailwind/build.clj
+++ ui-tailwind/build.clj
@@ -1,25 +1,48 @@
 (ns build
   (:require
    [clojure.tools.build.api :as b]
-   [org.corfield.build :as bb] ; https://github.com/seancorfield/build-clj
    [deps-deploy.deps-deploy :as dd]))
 
 
 (def lib 'org.pinkgorilla/ui-tailwind)
 (def version (format "0.1.%s" (b/git-count-revs nil)))
+(def class-dir "target/classes")
+(def basis (b/create-basis {:project "deps.edn"}))
+(def jar-file (format "target/%s-%s.jar" (name lib) version))
 
-(defn jar "build the JAR" [opts]
-  (println "Building the JAR")
-  (-> opts
-      (assoc :lib lib
-             :version version
-             :transitive true)
-      (bb/jar)))
+(defn clean [_]
+  (b/delete {:path "target"}))
 
+(def pom-template
+  [[:licenses
+    [:license
+     [:name "Eclipse Public License"]
+     [:url "https://www.eclipse.org/legal/epl-v10.html"]]]
+   [:developers
+    [:developer
+     [:name "pink-gorilla"]]]
+   [:scm
+    [:url "https://github.com/pink-gorilla/ui-tailwind/"]
+    [:connection "scm:git:git://github.com/pink-gorilla/ui-tailwind.git"]
+    [:developerConnection "scm:git:ssh://git@github.com/pink-gorilla/ui-tailwind.git"]]])
+(def opts {:class-dir class-dir
+           :lib lib
+           :version version
+           :basis basis
+           :pom-data pom-template
+           :src-dirs ["src"]})
 
-(defn deploy "Deploy the JAR to Clojars." [opts]
+(defn jar [_]
+  (b/write-pom opts)
+  (b/copy-dir {:src-dirs ["src" "resources"]
+               :target-dir class-dir})
+  (b/jar {:class-dir class-dir
+          :jar-file jar-file}))
+
+(defn deploy "Deploy the JAR to Clojars." [_]
   (println "Deploying to Clojars.")
-  (-> opts
-      (assoc :lib lib 
-             :version version)
-      (bb/deploy)))
+  (dd/deploy {:installer :remote
+              ;:sign-releases? true
+              :pom-file (b/pom-path (select-keys opts [:lib :class-dir]))
+              ;:artifact "target/tech.ml.dataset.jar"
+              :artifact (b/resolve-path jar-file)}))
